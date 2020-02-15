bbe95d06f053f1ddd9deee92bde6abc4e2bb0587
Maya <9sanchez@protonmail.com>
2020-02-15 04:36:49 -0800

    question and equipment code
    Added question and equipment code list  to page.

--- js/main.js
+++ js/main.js
@@ -286,10 +286,155 @@ window.addEventListener("load", function(){
   SUN: "283",
   THEATER: "284",
   WING: "285",
-  SWITCH: "286"
+  SWITCH: "286",
 };
+
+var equipmentCode = {
+  "HW R-D/W 5818MNL": "0470",
+  "Existing Glass Break Detector": "0475",
+  "HW Panic Pendant 5802MN2": "0491",
+  "HW Glass Break 5853": "0519",
+  "HW PIR 5894PI": "0530",
+  "HW PIR 5890": "0533",
+  "Existing Flood/Temp Sensor": "0556",
+  "HW Heat Sensor 5809": "0557",
+  "Existing Keyfob Remote": "0577",
+  "HW Smoke 5808W3": "0589",
+  "Existing Motion Detector": "0609",
+  "Existing Smoke Detector": "0616",
+  "HW Flood Sensor 5821": "0624",
+  "HWD/W 5816": "0637",
+  "Existing Door/Window Contact": "0655",
+  "Existing CO Detector": "0692",
+  "Existing Heat Sensor": "0708",
+  "CO1-345C CO Detector (Canada)": "0859",
+  "CO1-345 CO Detector (USA)": "0860",
+  "2GIG Thin Door/Window Contact": "0862",
+  "2GIG Recessed Door Contact": "0863",
+  "2GIG Glass Break Detector": "0864",
+  "2GIG 4-Button Keyfob Remote": "0866",
+  "2GIG Wireless Keypad": "0867",
+  "2GIG Panic Button Remote": "0868",
+  "2GIG PIR with Pet Immunity": "0869",
+  "SMKE1-345C Smoke Detector (Canada)": "0871",
+  "SMKE1-345 Smoke Detector (USA)": "0872",
+  "2GIG Takeover Module": "0873",
+  "SMTK2-345 GE Smoke/Heat Detector (USA/Canada)": "0895",
+  "2GIG CO Detector": "1026",
+  "2GIG Smoke Detector": "1058",
+  "2GIG-TS1 Wireless Touchscreen Keypad": "1059",
+  "Tilt Sensor": "1061",
+  "2GIG Tilt Sensor": "1062",
+  "2GIG Doorbell": "1063",
+  "2GIG Bypass Sensor": "1064",
+  "2GIG Flood Sensor": "1065",
+  "2GIG Shock Sensor": "1066",
+  "Alarm.com Image Sensor": "9999"
+};
+
+var questionCode = {
+Q1:"Select RF Sensor",
+Q2:"Select Wired Sensor",
+Q3:"Select Fob",
+Q4:"Select RF Keypad",
+Q5:"Enter Exit Delay",
+Q6:"Enter Entry Delay1",
+Q7:"Enter Entry Delay2",
+Q8:"Select Dialer",
+Q9:"Enter Dialing Prefix",
+Q10:"Enter Call Waiting DisableCode",
+Q11:"Enter CS#1 Phone Number",
+Q12:"Enter CS#1 Account Number",
+Q13:"Select Two‐Way Voice",
+Q14:"Select Silent Panic/Burglary Listen Only",
+Q15:"Select Dialing Type",
+Q16:"Select Police Emergency Key",
+Q17:"Select Fire Emergency Key",
+Q18:"Select Emergency Key",
+Q19:"Select Quick Arming",
+Q20:"Select Swinger Shutdown Count",
+Q21:"Select Siren Supervision Time",
+Q22:"Enter CS Lackof Usage Notification Time",
+Q23:"Enter Radio Modem Network Failure Time",
+Q24:"Select Radio Network Failure Causes Trouble",
+Q25:"Select Radio Modem Network Failure Reports",
+Q26:"Select Auto Stay",
+Q27:"Select Exit Delay Restart",
+Q28:"Select Quick Exit",
+Q29:"Enter Periodic Test",
+Q30: "?",
+Q31:"Enter Cancel Time",
+Q32:"Select Cancel Display",
+Q33:"Select Cross Sensor",
+Q34:"Enter Cross Sensor Timeout",
+Q35:"Select Abort Window Dialer Delay",
+Q36:"Select Burglary Bell Cutoff",
+Q37:"Select Fire Bell Cutoff",
+Q38:"Enter Time to Detect AC Loss",
+Q39:"Select Random AC Loss Report Time",
+Q40:"Enter CS#2 PhoneNumber",
+Q41:"Enter CS#2 Account Number",
+Q42:"Select Remote Control Phone",
+Q43:"Enter Installer Code",
+Q44:"Select Lock Installer Programming",
+Q45:"Select Lock Default Programming",
+Q46:"Select Trouble Doesn’t Sound at Night",
+Q47:"Select Troubles Resound After Hold off",
+Q48:"Enter Download CSID",
+Q49:"Select Programming Mode Entry Reportsto CS",
+Q50:"Select Trouble Reports to CS",
+Q51:"Select Manual Bypass Reports to CS",
+Q52:"Select AC Loss Reports to CS",
+Q53:"Select System Low Battery Reports to CS",
+Q54:"Select RF Low Battery Reports to CS",
+Q55:"Select OpeningReportstoCS",
+Q56:"Select Closing Reports to CS",
+Q57:"Select Alarm Restore Reports to CS",
+Q58:"Select Trouble Restore Reports to CS",
+Q59:"Select Bypass Restore Reports to CS",
+Q60:"Select AC Restore Reports to CS",
+Q61:"Select System Low Battery Restore Reports to CS",
+Q62:"Select RF Low Battery Restore Reports to CS",
+Q63:"Select Phone Fail Detect",
+Q64:"Select Smart Test Reports",
+Q65:"Select RF Jam Causes Trouble",
+Q66:"Select Daylight Saving",
+Q67:"Select Daylight Saving StartMonth",
+Q68:"Select Daylight Saving StartSunday",
+Q69:"Select Daylight Saving End Month",
+Q70:"Select Daylight Saving End Sunday",
+Q71:"Select System Tamper Causes Trouble",
+Q72:"Select Quick Bypass",
+Q73:"Select Disarming Keyfob After Alarm Alert",
+Q74:"Select Keyfob Arm/Disarm Confirmation",
+Q75:"Select Auto Unbypass for Manual Bypass",
+Q76:"Select Force Bypass Reports",
+Q77:"Select EventLog",
+Q78:"Select Output",
+Q79:"Select Z‐Wave Feature",
+Q80:"Select Z‐Wave Switches Feature",
+Q81:"Select Z‐Wave Thermostats Feature",
+Q82:"Select Z‐Wave Door Locks Feature",
+Q83:"Select Temperature Display Units",
+Q84:"Select Services Require Master Code",
+Q85:"Select Master User Access to Z‐Wave Toolbox",
+Q86:"Select Disable Siren After Two‐Way Audio",
+Q87:"Select Keyfob/Remote Arming Modeon System NotReady",
+Q88:"Select Z‐Wave Siren Mode",
+Q90:"Select Energy Feature",
+Q91:"Select Radio Modem Supplier Varies by Supplier",
+Q92:"Select Network Device",
+Q93:"Enter Broadband Network Failure Time",
+Q94:"Select Broadband Network Failure Causes Trouble",
+Q95:"Select Broadband Network Failure Reports",
+Q96:"Select send report 3 times on panel tamper",
+Q97:"Select soundon normal closing acknowledgment"
+}
   const code = document.getElementById('code');
   const keys = Object.keys(gigVoice).sort(); // just keys
+  const qCodeKeys = Object.keys(questionCode).sort();
+  const eCodeKeys = Object.keys(equipmentCode).sort();
+
   // const vals = Object.values(gigVoice); // just values of the keys
   // const objs = Object.entries(gigVoice); // all data
 
@@ -364,16 +509,19 @@ window.addEventListener("load", function(){
     return Array.from(new Set(letters));
 
   }
-
+    //Loop through object and get first letter of each key and display it in DOM.
     extractFirstLetter(keys).forEach((item) => {
-      alpha.innerHTML += "<a class='page-link'>" + item + "</a>";
+      alpha.innerHTML += "<a href='#voiceDescriptors' class='page-link'>" + item + "</a>";
     });
 
+    //gather location of where the alphabet letters will be displayed in DOM.
     var letterSelected = document.getElementById('alpha');
 
+    //function to process displayed words selected by end user.
     function displayWords(e){
-      console.log(e);
-      
+      //console out for testing.
+      // console.log(e);
+
       //gather location of where to display the list of words.
       var displayWords = document.getElementById('listWords');
 
@@ -384,18 +532,39 @@ window.addEventListener("load", function(){
       var char = e.target.innerText;
 
       //test output.
-      console.log('You selected ' + char);
+      // console.log('You selected ' + char);
 
       //iterate through keys object and find any matching letters end user has selected.
       keys.forEach(function(w, i){
         if(w.charAt(0) == char){
+          //console out put test.
           // console.log(w[i]);
           displayWords.innerHTML += '<li class="list-group-item">'+ w + ' : ' + gigVoice[w] + '</li>';
         }
       });
 
     }
-
+    //Listen for end user selection of voice descriptor letter.
     letterSelected.addEventListener('click',displayWords,false);
 
+    //gather location of where to display question and equipment codes list.
+    var qCodesList = document.getElementById("qCodesList");
+    var eCodesList = document.getElementById("eCodesList");
+
+    function displayQcode(){
+      qCodeKeys.forEach((q, i) => {
+        qCodesList.innerHTML += '<li class="list-group-item">' + q + ' : ' + questionCode[q] + '</li>';
+      });
+
+    }
+
+    function displayEcode(){
+      eCodeKeys.forEach((e, i) => {
+        eCodesList.innerHTML += '<li class="list-group-item">' + e + ' : ' + equipmentCode[e] + '</li>';
+      });
+
+    }
+
+    displayQcode();
+    displayEcode();
 });
